adc9eb6cf97666bd21bea21333cc58dee9541057
Sanpai <chandergaurav76@gmail.com>
2024-08-04 17:02:00 +0530

    first commit

---
+++ noise.js
@@ -0,0 +1,10 @@
+let options = {
+    "animate": true,
+    "patternWidth": 1920,
+    "patternHeight": 1080,
+    "grainOpacity": 1,
+    "grainDensity": 1,
+    "grainWidth": 1,
+    "grainHeight": 1,
+  }
+  grained("*", options);
