f2ae1a832f10edb264dd1bba274be103e2492705
Sanpai <chandergaurav76@gmail.com>
2024-08-22 11:21:55 +0530

    more changes

--- noise.js
+++ noise.js
@@ -1,10 +1,2 @@
-let options = {
-    "animate": true,
-    "patternWidth": 1920,
-    "patternHeight": 1080,
-    "grainOpacity": 1,
-    "grainDensity": 1,
-    "grainWidth": 1,
-    "grainHeight": 1,
-  }
-  grained("*", options);
+window.onload = function() 
+{ document.getElementById("hideAll").style.display = "none"; }
